e1cb385c8df3e6993117f3dcf8839eaa618102fa
Caleb Corbin <dawgsfan97@live.com>
2021-10-27 15:03:22 -0400

    added code for routing (it's not displaying tho)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import SignUp from './components/SignUp/SignUp';
-import PotencyCalculator from './components/PotencyCalculator/PotencyCalculator';
-import Merchandise from './components/Merchandise/Merchandise';
-import Cart from './components/Cart/Cart';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SignUp from "./components/SignUp/SignUp";
+import PotencyCalculator from "./components/PotencyCalculator/PotencyCalculator";
+import Merchandise from "./components/Merchandise/Merchandise";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <div className="App">
       <header className="my-4">
         <h1>Half Baked Kitchen</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/cart" component={Cart}>
+              Cart
+            </Route>
+            <Route exact path="/shop" component={Merchandise}>
+              Shop
+            </Route>
+            <Route exact path="/signup" component={SignUp}>
+              SignUp
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </header>
       <PotencyCalculator></PotencyCalculator>
     </div>
--- src/components/PotencyCalculator/PotencyCalculator.jsx
+++ src/components/PotencyCalculator/PotencyCalculator.jsx
@@ -1,79 +1,81 @@
-import React from 'react'
-import '../../App.css'
-import './PotencyCalculator.css';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import React from "react";
+import "../../App.css";
+import "./PotencyCalculator.css";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const PotencyCalculator = () => {
-    return (
-        <div>
-            <Container fluid className="mx-auto">
-                <Row className="justify-content-md-center">
-                    <Col>
-                        <Row className="my-2">
-                            <Col className="end-align">
-                                <label>Amount of Cannabis: </label>
-                            </Col>
-                            <Col className="start-align">
-                                <input type="decimal" size='10'/>
-                            </Col>
-                        </Row>
-                        <Row className="my-2">
-                            <Col className="end-align">
-                                <label>THC Percentage: </label>
-                            </Col>
-                            <Col className="start-align">
-                                <input type="decimal" size='10' />
-                            </Col>
-                        </Row>
-                        <Row className="my-2">
-                            <Col className="end-align">
-                                <label>Number of Servings: </label>
-                            </Col>
-                            <Col className="start-align">
-                                <input type="decimal" size='10' />
-                            </Col>
-                        </Row>
-                        <Row className="my-2">
-                            <Col className="end-align">
-                                <label>Type of oil: </label>
-                            </Col>
-                            <Col className="start-align">
-                                <select className="select-offset">
-                                    <option value="butter">Butter</option>
-                                    <option value="ghee">Ghee</option>
-                                    <option value="coconut-oil">Coconut Oil</option>
-                                    <option value="mct-oil">MCT Oil</option>
-                                    <option value="glycerin">Vegetable Glycerin</option>
-                                    <option value="ethanol">Ethanol</option>
-                                </select>
-                            </Col>
-                            </Row>
-                        <Row className="my-5">
-                            <Col>
-                                <Button variant="success" className="button-offset">Calculate Potency</Button>
-                            </Col>
-                        </Row>
-                        <Row className="my-2">
-                            <Col className="end-align">
-                                <label>THC per serving: </label>
-                            </Col>
-                            <Col className="center-align">
-                                <label id="servingTHC">0 mg</label>
-                            </Col>
-                        </Row>
-                        <Row className="my-2">
-                            <Col className="end-align">
-                                <label>Total THC: </label>
-                            </Col>
-                            <Col className="center-align">
-                                <label id="totalTHC">0 mg</label>
-                            </Col>
-                        </Row>
-                    </Col>
-                    </Row>
-            </Container>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Container fluid className="mx-auto">
+        <Row className="justify-content-md-center">
+          <Col>
+            <Row className="my-2">
+              <Col className="end-align">
+                <label>Amount of Cannabis: </label>
+              </Col>
+              <Col className="start-align">
+                <input type="decimal" size="10" className="input" />
+              </Col>
+            </Row>
+            <Row className="my-2">
+              <Col className="end-align">
+                <label>THC Percentage: </label>
+              </Col>
+              <Col className="start-align">
+                <input type="decimal" size="10" className="input" />
+              </Col>
+            </Row>
+            <Row className="my-2">
+              <Col className="end-align">
+                <label>Number of Servings: </label>
+              </Col>
+              <Col className="start-align">
+                <input type="decimal" size="10" className="input" />
+              </Col>
+            </Row>
+            <Row className="my-2">
+              <Col className="end-align">
+                <label>Type of oil: </label>
+              </Col>
+              <Col className="start-align">
+                <select className="select-offset">
+                  <option value="butter">Butter</option>
+                  <option value="ghee">Ghee</option>
+                  <option value="coconut-oil">Coconut Oil</option>
+                  <option value="mct-oil">MCT Oil</option>
+                  <option value="glycerin">Vegetable Glycerin</option>
+                  <option value="ethanol">Ethanol</option>
+                </select>
+              </Col>
+            </Row>
+            <Row className="my-5">
+              <Col>
+                <Button variant="success" className="button-offset">
+                  Calculate Potency
+                </Button>
+              </Col>
+            </Row>
+            <Row className="my-2">
+              <Col className="end-align">
+                <label>THC per serving: </label>
+              </Col>
+              <Col className="center-align">
+                <label id="servingTHC">0 mg</label>
+              </Col>
+            </Row>
+            <Row className="my-2">
+              <Col className="end-align">
+                <label>Total THC: </label>
+              </Col>
+              <Col className="center-align">
+                <label id="totalTHC">0 mg</label>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default PotencyCalculator
+export default PotencyCalculator;
